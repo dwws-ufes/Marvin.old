caecedd26f9a224367b85499c19b2274fcc5caaa
vitorsouza <vitorsouza@gmail.com>
2016-02-26 20:03:30 -0300

    Implemented randomized Marvin quotes at the home page.

--- src/br/ufes/inf/nemo/marvin/core/application/CoreInformation.java
+++ src/br/ufes/inf/nemo/marvin/core/application/CoreInformation.java
@@ -1,15 +1,23 @@
 package br.ufes.inf.nemo.marvin.core.application;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.inject.Named;
 
 import br.ufes.inf.nemo.marvin.core.domain.MarvinConfiguration;
 import br.ufes.inf.nemo.marvin.core.persistence.MarvinConfigurationDAO;
+import br.ufes.inf.nemo.util.ResourceUtil;
 import br.ufes.inf.nemo.util.ejb3.persistence.exceptions.PersistentObjectNotFoundException;
 
 /**
@@ -26,6 +34,9 @@ public class CoreInformation implements Serializable {
 
 	/** The logger. */
 	private static final Logger logger = Logger.getLogger(CoreInformation.class.getCanonicalName());
+
+	/** The path (in the EJB module) for the quotes file. */
+	private static final String QUOTES_FILE_PATH = "/META-INF/quotes.txt";
 	
 	/** TODO: document this field. */
 	private static final String DEFAULT_DECORATOR_NAME = "default";
@@ -43,8 +54,41 @@ public class CoreInformation implements Serializable {
 	/** Indicates if the system is properly installed. */
 	private Boolean systemInstalled;
 
-	/** Indicates the decorator being used in the administration area. */
+	/** Indicates the decorator being used. */
 	private String decorator = DEFAULT_DECORATOR_NAME;
+	
+	/** List of quotes. */
+	private List<String> quotes;
+	
+	/** Random number generator. */
+	private Random random = new Random(System.currentTimeMillis());
+	
+	/** Initializer method. */
+	@PostConstruct
+	private void init() {
+		// At first use, check if the system is installed.
+		logger.log(Level.FINER, "Checking if the system has been installed...");
+		
+		// If the system is configured, it's installed.
+		try {
+			currentConfig = marvinConfigurationDAO.retrieveCurrentConfiguration();
+			systemInstalled = true;
+		}
+		catch (PersistentObjectNotFoundException e) {
+			systemInstalled = false;
+		}
+		
+		// Load quotes.
+		quotes = new ArrayList<>();
+		File quotesFile = ResourceUtil.getResourceAsFile(QUOTES_FILE_PATH);
+		try (Scanner scanner = new Scanner(quotesFile)) {
+			while (scanner.hasNextLine()) quotes.add(scanner.nextLine().trim());
+		}
+		catch (IOException e) {
+			logger.log(Level.WARNING, "Could not load quotes from path: {0}", QUOTES_FILE_PATH);
+			quotes.add("I didn't ask to be made.");
+		}
+	}
 
 	/** Getter for currentConfig. */
 	public MarvinConfiguration getCurrentConfig() {
@@ -53,20 +97,6 @@ public class CoreInformation implements Serializable {
 
 	/** Getter for systemInstalled. */
 	public Boolean getSystemInstalled() {
-		// At first use, check if the system is installed.
-		if (systemInstalled == null) {
-			logger.log(Level.FINER, "Checking if the system has been installed...");
-			
-			// If the system is configured, it's installed.
-			try {
-				currentConfig = marvinConfigurationDAO.retrieveCurrentConfiguration();
-				systemInstalled = true;
-			}
-			catch (PersistentObjectNotFoundException e) {
-				systemInstalled = false;
-			}
-		}
-		
 		return systemInstalled;
 	}
 
@@ -74,5 +104,11 @@ public class CoreInformation implements Serializable {
 	public String getDecorator() {
 		if (! getSystemInstalled()) return NO_MENU_DECORATOR_NAME;
 		return decorator;
+	}
+	
+	/** Gets a random quote from Marvin. */
+	public String getQuote() {
+		int idx = random.nextInt(quotes.size());
+		return quotes.get(idx);
 	}
 }
