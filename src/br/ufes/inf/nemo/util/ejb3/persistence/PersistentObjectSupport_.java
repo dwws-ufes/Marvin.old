dcd5355c126d4cff9c200aecdad1b8f8a8e9847d
manzoli2122 <manzoli2122@gmail.com>
2016-05-01 15:58:59 -0300

    ok

--- src/br/ufes/inf/nemo/util/ejb3/persistence/PersistentObjectSupport_.java
+++ src/br/ufes/inf/nemo/util/ejb3/persistence/PersistentObjectSupport_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-03-11T12:28:07.158-0300")
+@Generated(value="Dali", date="2016-05-01T15:57:39.855-0300")
 @StaticMetamodel(PersistentObjectSupport.class)
 public class PersistentObjectSupport_ {
 	public static volatile SingularAttribute<PersistentObjectSupport, Long> id;
