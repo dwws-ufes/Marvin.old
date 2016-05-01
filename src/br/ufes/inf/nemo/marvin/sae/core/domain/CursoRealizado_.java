2eecf5f40385f76f8932cc73967eb8a522cff0d2
manzoli2122 <manzoli2122@gmail.com>
2016-05-01 15:42:52 -0300

    ok

--- src/br/ufes/inf/nemo/marvin/sae/core/domain/CursoRealizado_.java
+++ src/br/ufes/inf/nemo/marvin/sae/core/domain/CursoRealizado_.java
@@ -5,12 +5,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-04-23T08:35:54.817-0300")
+@Generated(value="Dali", date="2016-05-01T14:53:21.487-0300")
 @StaticMetamodel(CursoRealizado.class)
 public class CursoRealizado_ extends PersistentObjectSupport_ {
 	public static volatile SingularAttribute<CursoRealizado, String> matricula;
 	public static volatile SingularAttribute<CursoRealizado, Integer> anoInicio;
 	public static volatile SingularAttribute<CursoRealizado, Integer> anoTermino;
-	public static volatile SingularAttribute<CursoRealizado, Curso> curso;
 	public static volatile SingularAttribute<CursoRealizado, Egresso> egresso;
+	public static volatile SingularAttribute<CursoRealizado, Curso> curso;
 }
